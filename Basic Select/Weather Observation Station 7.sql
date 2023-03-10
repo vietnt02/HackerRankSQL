0dad5a75a70586a157d28b1a26750ebf8b99ce24
Nguyễn Tuấn Việt <127472325+vietnt02@users.noreply.github.com>
2023-03-10 13:44:08 +0700

    Update Weather Observation Station 7.sql

--- Basic Select/Weather Observation Station 7.sql
+++ Basic Select/Weather Observation Station 7.sql
@@ -2,7 +2,7 @@
 Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 */
 -- Solution
-SELECT
+SELECT DISTINCT
     CITY
 FROM
     STATION
